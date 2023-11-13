f910835c3296f5292bc55d72343d490e9a7b1b04
Victor DRUON <victor.druon@epfedu.fr>
2023-11-13 20:14:52 +0100

    Création des premieres class, ajout des premieres variables de la class tuile

---
+++ JeuLabyrinth/src/jeulabyrinth/Tuile.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package jeulabyrinth;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author vdruo
+ */
+public class Tuile {
+    
+    // attribut ways qui est une ArrayList, qui comporte toutes les directions de passage que la tuile peut permetre
+    ArrayList<String> ways; 
+    // attribut object qui est un String, indique quel objet est present sur la tuile, indique null si aucun objet est present
+    String object = null;
+    // attribut onBoard qui est un booleen, indique si la tuile est sur le plateau ou non
+    boolean onBoard = true;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
