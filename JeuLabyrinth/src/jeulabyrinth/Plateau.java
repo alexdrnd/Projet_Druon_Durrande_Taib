8ec50c85d84696818548ee4c030e78244435e8d6
Victor DRUON <victor.druon@epfedu.fr>
2023-11-15 01:58:45 +0100

    Avancement sur la classe Plateau : optimisation des methodes : ajout d'une methode dico qui retourne le dictionnaire des informations (pour ne pas prendre trop de place dans la methode ajouterTuilesDeJeu)

--- JeuLabyrinth/src/jeulabyrinth/Plateau.java
+++ JeuLabyrinth/src/jeulabyrinth/Plateau.java
@@ -39,22 +39,60 @@ public class Plateau {
     
     // methode qui cr?er et ajoute ? la liste l'ensemble des tuiles du jeu
     public void ajoutTuilesDeJeu() {
-        String[] DicoObjets = {"araignee","bague","bourse","carteTresor","chandelier",
-            "chauveSouris","chouette","cle","couronne","crane","dragon","epee","fantome",
-            "fee","genie","gobelin","heaume","lezard","livre","papillon","rat","saphir",
-            "scarabee","tresor","departB","departJ","departR","departV"};
         
+        String[][][][] DicoObjets = DicoObjets();
         
         // ajouter les tuiles "objet"
         for (int i=0 ; i<DicoObjets.length ; i++){
-            Tuile tuile = creerTuileObjet(DicoObjets[i]);
-            tuile.ajouterDirection("haut");
+            
+            Tuile tuile = creerTuileObjet(DicoObjets[i][0][0][0]);
+            
+            for (int j=1 ; j<4 ; j++) {
+                if (DicoObjets[i][0][0][j]!= null){
+                    tuile.ajouterDirection(DicoObjets[i][0][0][j]);
+                }
+            }
             TuilesDeJeu.add(tuile);
         }
-        
-        
     }
     
+    
+    // methode qui renvoit le dictionnaire comprenant les infos des object et des direction de chaque tuile
+    public String[][][][] DicoObjets(){
+        // dictionnaire
+        String[][][][] DicoObjets = {
+                                    {{{"araignee","haut","gauche",null}}}, 
+                                    {{{"bague","haut","bas","gauche"}}},
+                                    {{{"bourse","haut","droite","gauche"}}},
+                                    {{{"carteTresor","haut","bas","gauche"}}},
+                                    {{{"chandelier","droite","bas","gauche"}}},
+                                    {{{"chauveSouris","haut","droite","gauche"}}},
+                                    {{{"chouette","droite","bas",null}}},
+                                    {{{"cle","haut","droite","gauche"}}},
+                                    {{{"couronne","haut","bas","gauche"}}},
+                                    {{{"crane","haut","droite","bas"}}},
+                                    {{{"dragon","haut","droite","gauche"}}},
+                                    {{{"epee","haut","droite","bas"}}},
+                                    {{{"fantome","haut","droite","gauche"}}},
+                                    {{{"fee","haut","droite","gauche"}}},
+                                    {{{"genie","haut","droite","gauche"}}},
+                                    {{{"gobelin","haut","droite","gauche"}}},
+                                    {{{"heaume","droite","bas","gauche"}}},
+                                    {{{"lezard","haut","gauche",null}}},
+                                    {{{"livre","haut","droite","gauche"}}},
+                                    {{{"papillon","haut","gauche",null}}},
+                                    {{{"rat","bas","gauche",null}}},
+                                    {{{"saphir","haut","droite","bas"}}},
+                                    {{{"scarabee","haut","droite",null}}},
+                                    {{{"tresor","droite","bas","gauche"}}},
+                                    {{{"departB","droite","bas",null}}},
+                                    {{{"departJ","haut","droite",null}}},
+                                    {{{"departR","haut","gauche",null}}},
+                                    {{{"departV","bas","gauche",null}}}, 
+                                    };
+        
+        return DicoObjets;
+    }
     
     // methode pour creer une tuile de type "objet"
     public Tuile creerTuileObjet(String objet){
